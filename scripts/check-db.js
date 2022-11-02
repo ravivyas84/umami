b4d43cf65b9d750e1106ac75ec6e1708ef0c7887
ravivyas84 <ravivyas84@gmail.com>
2022-11-02 23:10:44 +0530

    Update check-db.js
    debugging

--- scripts/check-db.js
+++ scripts/check-db.js
@@ -34,7 +34,7 @@ async function checkConnection() {
 
     success('Database connection successful.');
   } catch (e) {
-    throw new Error('Unable to connect to the database.');
+    throw new Error('Unable to connect to the database. : ' + JSON.stringify(e);
   }
 }
 
